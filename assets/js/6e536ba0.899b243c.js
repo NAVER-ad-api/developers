bfa9c0a58ce3459bbe8b53c44c84f1812ed18c29
Hyungjun Kim <jun.k@navercorp.com>
2025-12-01 16:51:50 +0900

    Deploy website - based on e7fc40907a7c5b501e6ffb7bb6c5e039cfecfb2f

---
+++ assets/js/6e536ba0.899b243c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpa_openapi_docs=self.webpackChunkpa_openapi_docs||[]).push([[2685],{6258:e=>{e.exports=JSON.parse('{"permalink":"/developers/blog/tags/patch","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
--- en/assets/js/0fff602b.7a289923.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkpa_openapi_docs=self.webpackChunkpa_openapi_docs||[]).push([[1701],{1410:e=>{e.exports=JSON.parse('{"permalink":"/developers/en/blog/tags/patch","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
